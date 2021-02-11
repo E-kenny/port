e2f7cf7c97d1fa70441311f62e9b2c48f078ca34
Ekene <ekennyobiasogu@gmail.com>
2021-02-11 10:51:47 +0100

    feedback

--- index.js
+++ index.js
@@ -1,18 +1,4 @@
 function sendEmail(){
     let body = document.getElementById("body").value
-    let name = document.getElementById("name").value
-    window.open('mailto:Ekennyobiasogu@gmail.com?subject=name&body=body')
-    // sendEmail.send({
-    //     Host : "smtp.gmail.com",
-    //     Username : "mathewobiasogu@gmail.com",
-    //     password : "mathew2468",
-    //     To : "ekennyobiasogu@gmail.com",
-    //     From : "mathewobiasogu@gmail.com",
-    //     Subject : name,
-    //     Body : body,
-    // }).then(
-    //     message => alert("mail sent successfully")
-    // );
-    alert("what's up")
-
+    window.open('mailto:Ekennyobiasogu@gmail.com?'+'body='+body)
 }
